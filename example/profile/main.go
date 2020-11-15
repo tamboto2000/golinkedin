df8e2e269246790070c6c3293532245bd320475d
Franklin Collin Tamboto <tamboto2000@gmail.com>
2020-11-15 13:58:27 +0700

    Add examples

---
+++ example/profile/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/tamboto2000/linkedin"
+)
+
+func main() {
+	ln := linkedin.New()
+	ln.SetCookieStr(`your_linkedin_cookies`)
+
+	profile, err := ln.ProfileByUsername("linkedin_username")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	f, err := os.Create("profile.json")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if err := json.NewEncoder(f).Encode(profile); err != nil {
+		panic(err.Error())
+	}
+}
